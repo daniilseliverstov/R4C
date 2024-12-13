80f23e1ae7f3c2710f48ce6711b741a10d6040c5
daniilseliverstov <selivrstov91@internet.ru>
2024-12-13 20:02:59 +0300

    task2

--- robots/views.py
+++ robots/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from openpyxl import Workbook
+from .models import Robot
+from datetime import datetime, timedelta
 
-# Create your views here.
+
+def get_robot_production_data():
+    today = datetime.today()
+    last_week = today - timedelta(days=7)
+    robots = Robot.objects.filter(created__gte=last_week)
+    data = {}
+    for robot in robots:
+        model = robot.model
+        version = robot.version
+        if model not in data:
+            data[model] = {}
+        if version not in data[model]:
+            data[model][version] = 0
+        data[model][version] += 1
+    return data
+
+
+def excel_view(request):
+    data = get_robot_production_data()
+    wb = Workbook()
+    ws = wb.active
+    for model, versions in data.items():
+        ws.append([model])
+        for version, count in versions.items():
+            ws.append([version, count])
+        ws.append([])
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename=robot_production.xlsx'
+    wb.save(response)
+    return response
